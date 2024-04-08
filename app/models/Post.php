78438d5544565873291103be7b8e39b39d12f398
fkhan67 <157556820+fkhan67@users.noreply.github.com>
2024-04-08 13:42:55 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,7 +2,44 @@
 
 namespace app\models;
 
-class Post
-{
-    //todo make methods here
+use PDO;
+
+class Post {
+    private $pdo;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function getAllPosts() {
+        $query = "SELECT * FROM posts";
+        $statement = $this->pdo->query($query);
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function savePost($title, $description) {
+        $query = "INSERT INTO posts (title, description) VALUES (?, ?)";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([$title, $description]);
+        return $this->pdo->lastInsertId();
+    }
+
+    public function getPostById($id) {
+        $query = "SELECT * FROM posts WHERE id = ?";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([$id]);
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updatePost($id, $title, $description) {
+        $query = "UPDATE posts SET title = ?, description = ? WHERE id = ?";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([$title, $description, $id]);
+    }
+
+    public function deletePost($id) {
+        $query = "DELETE FROM posts WHERE id = ?";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute([$id]);
+    }
 }
