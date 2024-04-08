0ba31115d9aa528afbb5ca63e4cbf217324c2a8f
fkhan67 <157556820+fkhan67@users.noreply.github.com>
2024-04-08 13:39:46 -0400

    Update question-3-answers.sql

--- sql/question-3-answers.sql
+++ sql/question-3-answers.sql
@@ -1,16 +1,19 @@
--- select by id 1:
+-- Select a post by id = 1
+SELECT * FROM posts WHERE id = 1;
 
+-- Select posts where title includes “title 2”
+SELECT * FROM posts WHERE title LIKE '%title 2%';
 
--- select all posts where title = includes "title 2":
+-- Select and order posts alphabetically by name
+SELECT * FROM posts ORDER BY title ASC;
 
+-- Write an insert statement to make 3 new posts
+INSERT INTO posts (title, content) VALUES ('New Post 1', 'Content of new post 1');
+INSERT INTO posts (title, content) VALUES ('New Post 2', 'Content of new post 2');
+INSERT INTO posts (title, content) VALUES ('New Post 3', 'Content of new post 3');
 
--- select all posts and order by the title column alphabetically:
+-- Write an update statement to update post with id = 1 and change the name to “Updated Post”
+UPDATE posts SET title = 'Updated Post' WHERE id = 1;
 
-
--- insert 3 new posts
-
-
--- update posts where id = 1, set the title to "new title"
-
-
--- delete post where id = 2
+-- Write a delete statement to delete post id = 2
+DELETE FROM posts WHERE id = 2;
